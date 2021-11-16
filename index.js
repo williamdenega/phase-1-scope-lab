cabde3b90fd7220fd16de57ba105a73f24068817
WilliamDenega <williamdenega@gmail.com>
2021-11-16 18:00:26 -0500

    commit tree 11f1ba7ef27e8b606a42fbcaf400f15c0225bbae

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+
+var customerName = 'bob';
+const leastFavoriteCustomer = "greg";
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'billy';
+
+}
